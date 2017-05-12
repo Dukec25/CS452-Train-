0155ff6a964b65004516ca06077bf195ad099f95
Alicia Hu <a7hu@uwaterloo.ca>
2017-05-12 15:49:42 -0400

    Kernel1: copy over common macros

---
+++ common/include/define.h
@@ -0,0 +1,19 @@
+#ifndef __DEFINE_H__
+#define __DEFINE_H__
+
+/* typedefs */
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+typedef unsigned long long uint64;
+typedef volatile int vint;
+typedef unsigned int size_t;
+
+/* defines */
+#define COM1	1
+#define COM2	2
+#define ON	1
+#define	OFF	0
+#define NULL	0
+
+#endif //__DEFINE_H__
