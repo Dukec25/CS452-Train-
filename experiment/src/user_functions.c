9f0f71ba03762122ab26df91a515ed0b5189c51a
Alicia Hu <a7hu@uwaterloo.ca>
2017-05-22 14:57:21 -0400

    Add debug level, resolve warning msg by changing .h and declare asm_* in .c

--- experiment/src/user_functions.c
+++ experiment/src/user_functions.c
@@ -1,45 +1,52 @@
-#include <bwio.h>
+#include <debug.h>
 #include <user_functions.h>
+
+extern void asm_init_kernel();
+extern int asm_kernel_create(int priority, void (*code) ());
+extern void asm_kernel_pass();
+extern void asm_kernel_exit();
+extern int asm_kernel_my_tid();
+extern int asm_kernel_my_parent_tid();
 
 void init_kernel()
 {
-    debug("In user mode %s", "init_kernel");
-    asm_init_kernel();
+	debug(DEBUG_SYSCALL, "In user mode %s", "init_kernel");
+	asm_init_kernel();
 }
 
 // upper case due to A1 specification 
 int Create(int priority, void (*code) ())
 {
-    debug("In user mode Create, priority = %d, code = 0x%x\n", priority, code);
-    vint tid = asm_kernel_create(priority, code);
-    debug("In user mode Create, tid = %d", tid);
-    return tid;
+	debug(DEBUG_SYSCALL, "In user mode Create, priority = %d, code = 0x%x", priority, code);
+	vint tid = asm_kernel_create(priority, code);
+	debug(DEBUG_SYSCALL, "In user mode Create, tid = %d", tid);
+	return tid;
 }
 
 void Pass()
 {
-	debug("In user mode %s", "Pass");
-    asm_kernel_pass();
+	debug(DEBUG_SYSCALL, "In user mode %s", "Pass");
+	asm_kernel_pass();
 }
 
 void Exit()
 {
-	debug("In user mode %s", "Exit");
-    asm_kernel_exit();
+	debug(DEBUG_SYSCALL, "In user mode %s", "Exit");
+	asm_kernel_exit();
 }
 
 int MyTid()
 {
-	debug("In user mode %s", "MyTid");
-    vint tid = asm_kernel_my_tid();
-	debug("In user mode MyTid tid = %d", tid);
+	debug(DEBUG_SYSCALL, "In user mode %s", "MyTid");
+	vint tid = asm_kernel_my_tid();
+	debug(DEBUG_SYSCALL, "In user mode MyTid tid = %d", tid);
 	return tid;
 }
 
 int MyParentTid()
 {
-	debug("In user mode %s", "MyParentTid");
-    vint tid = asm_kernel_my_parent_tid();
-	debug("In user mode MyParentTid tid = %d", tid);
+	debug(DEBUG_SYSCALL, "In user mode %s", "MyParentTid");
+	vint tid = asm_kernel_my_parent_tid();
+	debug(DEBUG_SYSCALL, "In user mode MyParentTid tid = %d", tid);
 	return tid;
 }
--- include/user_functions.h
+++ include/user_functions.h
@@ -1,3 +1,9 @@
+#ifndef __USER_FUNCTIONS_H__
+#define __USER_FUNCTIONS_H__
 void init_kernel();
-int create( int priority, void (*code) (  )  );
-void pass();
+int Create(int priority, void (*code) ());
+void Pass();
+void Exit();
+int MyTid();
+int MyParentTid();
+#endif // __USER_FUNCTIONS_H__
