301f55726cc92eb1859442fd71811e450b8ad432
Alicia Hu <a7hu@uwaterloo.ca>
2017-05-22 14:59:05 -0400

    Add debug level

--- experiment/src/kernel.c
+++ experiment/src/kernel.c
@@ -1,36 +1,42 @@
 #include <kernel.h>
-#include <bwio.h>
+#include <debug.h>
 
-void k_init_kernel(){
-    bwprintf(COM2, "in the kernel");
+void k_init_kernel()
+{
+	debug(DEBUG_SYSCALL, "In %s", "k_init_kernel");
+	bwprintf(COM2, "in the kernel");
 }
-
+ 
 void k_create(void (*task)(), kernel_state *ks, uint32 tid, uint32 ptid, task_priority priority)
 {
-	debug("In kernel mode k_create, tid = %d", tid);
+	debug(DEBUG_SYSCALL, "In kernel mode k_create, tid = %d", tid);
 	td_intialize(task, ks, tid, ptid, priority);
 	ks->tasks[ptid].retval = tid;
 }
 
-void k_my_tid(task_descriptor *td){
-	debug("In kernel mode k_my_tid, td->tid = %d", td->tid);
-    td->retval = td->tid;
+void k_my_tid(task_descriptor *td)
+{
+	debug(DEBUG_SYSCALL, "In kernel mode k_my_tid, td->tid = %d", td->tid);
+	td->retval = td->tid;
 }
 
-void k_my_parent_tid(task_descriptor *td){
-	debug("In kernel mode k_my_parent_tid, td->ptid = %d", td->ptid);
-    td->retval = td->ptid;
+void k_my_parent_tid(task_descriptor *td)
+{
+	debug(DEBUG_SYSCALL, "In kernel mode k_my_parent_tid, td->ptid = %d", td->ptid);
+	td->retval = td->ptid;
 }
 
-void k_pass(task_descriptor *td, kernel_state *ks) {
-	debug("In %s", "k_pass");
-    remove_task(td, ks);
-    td->state = STATE_READY;
-    insert_task(td, ks);
+void k_pass(task_descriptor *td, kernel_state *ks)
+{
+	debug(DEBUG_SYSCALL, "In %s", "k_pass");
+	remove_task(td, ks);
+	td->state = STATE_READY;
+	insert_task(td, ks);
 }
 
-void k_exit(task_descriptor *td, kernel_state *ks) {
-	debug("In %s", "k_exit");
-    td->state = STATE_ZOMBIE;
-    remove_task(td, ks);
+void k_exit(task_descriptor *td, kernel_state *ks)
+{
+	debug(DEBUG_SYSCALL, "In %s", "k_exit");
+	td->state = STATE_ZOMBIE;
+	remove_task(td, ks);
 }
--- experiment/src/user_tasks.c
+++ experiment/src/user_tasks.c
@@ -1,28 +1,29 @@
 #include <kernel.h>
-#include <bwio.h>
-void general_task(){
-	debug("In user task %s", "general_task");
-    uint32 tid = MyTid();
-    uint32 ptid = MyParentTid();
-    debug("Get back to user task general_task before pass, tid = %d, ptid = %d", tid, ptid);
-    Pass();
-    debug("Get back to user task general_task after pass, tid = %d, ptid = %d", tid, ptid);
-    Exit();
+#include <user_functions.h>
+#include <debug.h>
+
+void general_task()
+{
+	debug(DEBUG_TASK, "In user task %s", "general_task");
+	uint32 tid = MyTid();
+	uint32 ptid = MyParentTid();
+	debug(DEBUG_TASK, "Get back to user task general_task before pass, tid = %d, ptid = %d", tid, ptid);
+	Pass();
+	debug(DEBUG_TASK, "Get back to user task general_task after pass, tid = %d, ptid = %d", tid, ptid);
+	Exit();
 }
 
-void first_task(){
-	debug("In user task first_task, priority=%d", PRIOR_MEDIUM);
-    int tid = Create(PRIOR_LOW, general_task);
-	debug("created taskId = %d", tid);
-    tid = Create(PRIOR_LOW, general_task);
-    debug("created taskId = %d", tid);
-    /*tid = Create(PRIOR_HIGH, general_task);*/
-    /*debug("created taskId = %d", tid);*/
-    /*tid = Create(PRIOR_HIGH, general_task);*/
-    /*debug("created taskId = %d", tid);*/
-	debug("%s", "FirstUserTask: exiting");
-    Exit();
+void first_task()
+{
+	debug(DEBUG_TASK, "In user task first_task, priority=%d", PRIOR_MEDIUM);
+	int tid = Create(PRIOR_LOW, general_task);
+	debug(DEBUG_TASK, "created taskId = %d", tid);
+	tid = Create(PRIOR_LOW, general_task);
+	debug(DEBUG_TASK, "created taskId = %d", tid);
+	/*tid = Create(PRIOR_HIGH, general_task);*/
+	/*debug("created taskId = %d", tid);*/
+	/*tid = Create(PRIOR_HIGH, general_task);*/
+	/*debug("created taskId = %d", tid);*/
+	debug(DEBUG_TASK, "%s", "FirstUserTask: exiting");
+	Exit();
 }
-
-
-
