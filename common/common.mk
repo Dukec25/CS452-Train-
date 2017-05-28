2329a834aaded71416fa698236aed9e6c37a1187
Alicia Hu <a7hu@uwaterloo.ca>
2017-05-27 20:08:42 -0400

    Tier makefile changes

--- common/common.mk
+++ common/common.mk
@@ -13,7 +13,7 @@ ARFLAGS = rcs
 
 INCLUDE = -I../../common/include -I../../util/include -I../../io/include
 LDPATHS = -L/u/wbcowan/gnuarm-4.0.2/lib/gcc/arm-elf/4.0.2 -L../../common/lib -L../../io/lib -L../../util/lib 
-LIBS 	= -lutil -lc -lgcc
+LIBS 	= -lutil -lc -lgcc -lcommon
 LDFLAGS = -init main -Map $(MAP) -N  -T orex.ld $(LDPATHS) $(LIBS)
 
 BUILD	= build
