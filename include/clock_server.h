70cda818a1f70a47be0572c057fd7dba6884c346
c25du <changqi.du@uwaterloo.ca>
2017-06-03 21:11:32 -0400

    add the missing clock server.h file

---
+++ include/clock_server.h
@@ -0,0 +1,43 @@
+#ifndef __CLOCK_SERVER_H__
+#define __CLOCK_SERVER_H__
+
+#include <fifo.h>
+#include <kernel.h>
+#include <define.h>
+#include <name_server.h>
+#include <heap.h>
+
+#define MAX_DELAYED_TASKS	50
+
+typedef enum Clock_message_type {
+    CLOCK_NOTIFIER,
+    TIME_REQUEST,
+    DELAY_REQUEST,
+    DELAY_REQUEST_UNTIL
+} Clock_message_type;
+
+typedef struct Clock_server_message {
+	Clock_message_type type;
+    vint ticks; 
+} Clock_server_message;
+
+typedef struct Clock_server {
+	vint ticks;
+	//node_t delayed_tasks[MAX_DELAYED_TASKS];
+} Clock_server;
+
+typedef struct Delayed_task {
+	vint tid;
+    vint freedom_tick;
+} Delayed_task;
+
+typedef struct Delivery{
+    vint type;
+    vint data;
+} Delivery;
+
+int DelayUntil( int ticks );
+int Delay( int ticks );
+int Time();
+
+#endif // __CLOCK_SERVER_H__
