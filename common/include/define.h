74b392b69c6078aa19db2fe0dbb5706f90b41c91
Alicia Hu <a7hu@uwaterloo.ca>
2017-06-26 03:07:16 -0400

    Logger implementation

--- common/include/define.h
+++ common/include/define.h
@@ -1,12 +1,25 @@
 #ifndef __DEFINE_H__
 #define __DEFINE_H__
 
+typedef char *va_list;
+
+#define __va_argsiz(t)	\
+		(((sizeof(t) + sizeof(int) - 1) / sizeof(int)) * sizeof(int))
+
+#define va_start(ap, pN) ((ap) = ((va_list) __builtin_next_arg(pN)))
+
+#define va_end(ap)	((void)0)
+
+#define va_arg(ap, t)	\
+		 (((ap) = (ap) + __va_argsiz(t)), *((t*) (void*) ((ap) - __va_argsiz(t))))
+
 /* typedefs */
 typedef unsigned char uint8;
 typedef unsigned short uint16;
 typedef unsigned int uint32;
 typedef unsigned long long uint64;
 typedef volatile int vint;
+typedef volatile char vchar;
 typedef unsigned int size_t;
 
 /* defines */
@@ -18,4 +31,6 @@ typedef unsigned int size_t;
 
 /* debug */
 #define DEBUG	1400
+#define LOG		13
+
 #endif //__DEFINE_H__
--- util/include/bwio.h
+++ util/include/bwio.h
@@ -1,18 +1,6 @@
 #ifndef __BWIO_H__
 #define __BWIO_H__
 #include <define.h>
-typedef char *va_list;
-
-#define __va_argsiz(t)	\
-		(((sizeof(t) + sizeof(int) - 1) / sizeof(int)) * sizeof(int))
-
-#define va_start(ap, pN) ((ap) = ((va_list) __builtin_next_arg(pN)))
-
-#define va_end(ap)	((void)0)
-
-#define va_arg(ap, t)	\
-		 (((ap) = (ap) + __va_argsiz(t)), *((t*) (void*) ((ap) - __va_argsiz(t))))
-
 /* Busy-wait I/O */
 int bwsetfifo(int channel, int state);
 int bwsetspeed(int channel, int speed);
