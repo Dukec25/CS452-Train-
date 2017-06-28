a60fde8c0550df0d8f336cd777d16386467f7ad9
c25du <changqi.du@uwaterloo.ca>
2017-06-27 21:28:38 -0400

    Added the comment for find_stops_by_distance

--- common/include/calculation.h
+++ common/include/calculation.h
@@ -23,6 +23,14 @@ typedef struct Sensor_dist {
     int distance;
 } Sensor_dist;
 
+
+/*
+ *  Return the fifo which contains all the stops on the way of
+ *  stop_distance + dest (excluding dest sensors and include the 
+ *  sensor that is closest to the stop_location but not on the way)  
+ *
+ *  the first value in the fifo is the closest with the stop
+ */
 fifo_t find_stops_by_distance(track_node *track, int src, int dest, int stop_distance);
 
 #endif //__CALCULATION_H__
