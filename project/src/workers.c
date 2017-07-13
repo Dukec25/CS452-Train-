9fb476c5003bfd580823e39a2a6eaf642720364b
c25du <changqi.du@uwaterloo.ca>
2017-07-13 12:00:25 -0400

    Forget to include the new files for the previous commit

---
+++ project/src/workers.c
@@ -0,0 +1,33 @@
+#include <workers.h>
+
+void delay_task()
+{
+	Handshake handshake = HANDSHAKE_AKG;
+	int train_server_tid = INVALID_TID;
+	vint train_server_address;
+	Receive(&train_server_tid, &train_server_address, sizeof(train_server_address));
+	Reply(train_server_tid, &handshake, sizeof(handshake));
+	Train_server *train_server = (Train_server *) train_server_address;
+	
+	while (train_server->is_shutdown == 0) {
+        int requester_tid;
+        Delay_request delay_req;
+
+        Receive(&requester_tid, &delay_req, sizeof(delay_req));
+        Reply(requester_tid, &handshake, sizeof(handshake));
+
+        Delay(delay_req.delay_time);
+
+		TS_request ts_request;
+		ts_request.type = TS_DELAY_TIME_UP;
+
+		Send(train_server_tid, &ts_request, sizeof(ts_request), &handshake, sizeof(handshake));
+	}
+
+	Handshake exit_handshake = HANDSHAKE_SHUTDOWN;
+	Handshake exit_reply;
+	Send(train_server_tid, &exit_handshake, sizeof(exit_handshake), &exit_reply, sizeof(exit_reply)); 
+	
+	Exit();
+}
+
